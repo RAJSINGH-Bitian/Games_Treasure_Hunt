5578e60e5dbc639e72bce9dfed09708556f46686
RAJSINGH-Bitian <rahul545fd@gmail.com>
2021-07-14 11:11:18 +0530

    my first project

---
+++ app/src/main/java/com/example/android/treasureHunt/GeofenceUtils.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.treasureHunt
+
+import android.content.Context
+import com.google.android.gms.location.GeofenceStatusCodes
+import com.google.android.gms.maps.model.LatLng
+import java.util.concurrent.TimeUnit
+
+/**
+ * Returns the error string for a geofencing error code.
+ */
+fun errorMessage(context: Context, errorCode: Int): String {
+    val resources = context.resources
+    return when (errorCode) {
+        GeofenceStatusCodes.GEOFENCE_NOT_AVAILABLE -> resources.getString(
+            R.string.geofence_not_available
+        )
+        GeofenceStatusCodes.GEOFENCE_TOO_MANY_GEOFENCES -> resources.getString(
+            R.string.geofence_too_many_geofences
+        )
+        GeofenceStatusCodes.GEOFENCE_TOO_MANY_PENDING_INTENTS -> resources.getString(
+            R.string.geofence_too_many_pending_intents
+        )
+        else -> resources.getString(R.string.unknown_geofence_error)
+    }
+}
+
+/**
+ * Stores latitude and longitude information along with a hint to help user find the location.
+ */
+data class LandmarkDataObject(val id: String, val hint: Int, val name: Int, val latLong: LatLng)
+
+internal object GeofencingConstants {
+
+    /**
+     * Used to set an expiration time for a geofence. After this amount of time, Location services
+     * stops tracking the geofence. For this sample, geofences expire after one hour.
+     */
+    val GEOFENCE_EXPIRATION_IN_MILLISECONDS: Long = TimeUnit.HOURS.toMillis(1)
+
+    val LANDMARK_DATA = arrayOf(
+        LandmarkDataObject(
+            "golden_gate_bridge",
+            R.string.golden_gate_bridge_hint,
+            R.string.golden_gate_bridge_location,
+            LatLng(23.79352129146, 86.36395586728993)),
+
+        LandmarkDataObject(
+            "ferry_building",
+            R.string.ferry_building_hint,
+            R.string.ferry_building_location,
+            LatLng(23.78826207460077, 86.41765572681234)),
+
+        LandmarkDataObject(
+            "pier_39",
+            R.string.pier_39_hint,
+            R.string.pier_39_location,
+            LatLng(23.800157413217097, 86.42635125917431)),
+
+        LandmarkDataObject(
+           "union_square",
+            R.string.union_square_hint,
+            R.string.union_square_location,
+            LatLng(23.78865481415778, 86.41990943042794))
+    )
+
+    val NUM_LANDMARKS = LANDMARK_DATA.size
+    const val GEOFENCE_RADIUS_IN_METERS = 100f
+    const val EXTRA_GEOFENCE_INDEX = "GEOFENCE_INDEX"
+}
